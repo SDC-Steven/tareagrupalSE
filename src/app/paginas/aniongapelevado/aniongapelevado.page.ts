d67ebfaae8a09dddeef790c2c6481249f11c7911
Brenedin <brenedingomez@protonmail.com>
2022-04-08 00:10:40 -0600

    imagenes ordenadas y checkbox listo

--- src/app/paginas/aniongapelevado/aniongapelevado.page.ts
+++ src/app/paginas/aniongapelevado/aniongapelevado.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-aniongapelevado',
   templateUrl: './aniongapelevado.page.html',
   styleUrls: ['./aniongapelevado.page.scss'],
 })
-export class AniongapelevadoPage implements OnInit {
+export class AniongapelevadoPage  {
 
-  constructor() { }
+  @ViewChild('cardHipo',{read:ElementRef, static:false}) chipoxia: ElementRef;
+
+  @ViewChild('cardCeto',{read:ElementRef, static:false}) ccetonico: ElementRef;
+
+  @ViewChild('cardRenal',{read:ElementRef, static:false}) crenal: ElementRef;
+ 
+  constructor(private renderer:Renderer2, private element:ElementRef) {
+    
+   }
 
   ngOnInit() {
+   
   }
+
+  
+  
+  onCheckHipo() {
+    const chipo =  this.chipoxia.nativeElement;
+    console.log(this.chipoxia);
+    console.log(this.chipoxia.nativeElement);
+     this.renderer.setStyle(this.chipoxia.nativeElement, 'display','block');
+    
+   }
+ 
+   onCheckCeto(){
+     this.renderer.setStyle(this.ccetonico.nativeElement, 'display','block');
+   }
+ 
+   onCheckRenal(){
+     this.renderer.setStyle(this.crenal.nativeElement, 'display','block');
+   }
 
 }
